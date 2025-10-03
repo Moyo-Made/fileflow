51a672101eecb9e41674903007687494865c8157
Moyo-Made <adegbitemoyomade2004@gmail.com>
2025-10-02 21:04:15 -0400

    feat: create electron hello world app

---
+++ index.js
@@ -0,0 +1,36 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadFile('index.html');
+  
+  // Open DevTools in development
+  if (process.argv.includes('--dev')) {
+    win.webContents.openDevTools();
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
